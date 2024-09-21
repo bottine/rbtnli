ec15fab41a723fb1770c0ba235720fd79a7f898f
Rémi Bottinelli <bottine@users.noreply.github.com>
2024-09-21 04:54:19 +0000

    Create pandoc_all.py

---
+++ pandoc_all.py
@@ -0,0 +1,8 @@
+import glob
+import os
+
+for f in glob.glob('**/*.md', recursive=True):
+    pre, ext = os.path.splitext(f)
+    outf = "output/" + pre + ".html"
+    os.makedirs(os.path.dirname(outf), exist_ok=True)
+    os.system(f"pandoc -i \'{f}\' -o \'{outf}\'") 
