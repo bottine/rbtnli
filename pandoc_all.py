9d751288966f7e86881a256746248e441b1aee9a
Rémi Bottinelli <remi.bottinelli@bluewin.ch>
2024-09-21 08:13:55 +0200

    mmh

--- pandoc_all.py
+++ pandoc_all.py
@@ -5,4 +5,4 @@ for f in glob.glob('**/*.md', recursive=True):
     pre, ext = os.path.splitext(f)
     outf = "output/" + pre + ".html"
     os.makedirs(os.path.dirname(outf), exist_ok=True)
-    os.system(f"pandoc/core -i \'{f}\' -o \'{outf}\'") 
+    os.system(f"pandoc -i \'{f}\' -o \'{outf}\'") 
